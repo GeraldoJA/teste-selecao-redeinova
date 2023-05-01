d7975e7ca3590b05c193e1ef26d93d8553025d67
Geraldo <geraldo.gja@gmail.com>
2023-05-01 12:56:50 -0300

    BackEnd: adicionar documentação swagger

--- backend/src/main/java/exercicio/java/angular/backend/documentos/controller/HistoricoController.java
+++ backend/src/main/java/exercicio/java/angular/backend/documentos/controller/HistoricoController.java
@@ -3,7 +3,9 @@ package exercicio.java.angular.backend.documentos.controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import exercicio.java.angular.backend.documentos.model.Historico;
-import exercicio.java.angular.backend.documentos.repository.HistoricoRepository;
+import exercicio.java.angular.backend.documentos.model.Documento;
+import exercicio.java.angular.backend.documentos.model.Situacao;
+import exercicio.java.angular.backend.documentos.service.IHistoricoService;
 import java.util.List;
 
 @CrossOrigin
@@ -12,11 +14,18 @@ import java.util.List;
 public class HistoricoController {
 
     @Autowired
-    private HistoricoRepository repository;
+    private IHistoricoService service;
 
     @GetMapping("{id}")
     public List<Historico> listaHistoricoDocumento(@PathVariable Long id) {
-        return repository.listaHistoricoDocumento(id);
+        return service.listaHistoricoDocumento(id);
+    }
+
+    @PostMapping
+    public Historico insert(           
+            @RequestBody Documento documento,
+            @RequestBody Situacao situacao) {
+        return service.insert(documento, situacao);
     }
     
 }
