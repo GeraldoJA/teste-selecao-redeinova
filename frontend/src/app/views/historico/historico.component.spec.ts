11f6a814233b0c0f11be0561a437d6cd149364e6
Geraldo <geraldo.gja@gmail.com>
2023-05-03 14:42:07 -0300

    FrontEnd: aumentar a cobertura de testes [refatoração]

--- frontend/src/app/views/historico/historico.component.spec.ts
+++ frontend/src/app/views/historico/historico.component.spec.ts
@@ -1,1 +1,27 @@
 
+import {ComponentFixture, TestBed} from '@angular/core/testing';
+
+import {HistoricoComponent} from './historico.component';
+import {AppModule} from "../../app.module";
+
+describe('HistoricoComponent', () => {
+  let component: HistoricoComponent;
+  let fixture: ComponentFixture<HistoricoComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [
+        AppModule
+      ]
+    })
+      .compileComponents();
+
+    fixture = TestBed.createComponent(HistoricoComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
